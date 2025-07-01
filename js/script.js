4c106dabac45998c904c3efc875cee3dfc6d48b9
Nazlaiftitah <iftitahnazla@gmail.com>
2025-07-01 15:44:51 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // toggle class active
 const navLinks = document.querySelector(".navbar-nav");
-// ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarnav.classlist.toggle("active");
-};
-
-// klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
+// ketika hamburger menu diklik
+hamburger.onclick = () => {
+  navLinks.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menutup navbar
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
-    navbarnav.classlist.remove("active");
+  if (!hamburger.contains(e.target) && !navLinks.contains(e.target)) {
+    navLinks.classList.remove("active");
   }
 });
